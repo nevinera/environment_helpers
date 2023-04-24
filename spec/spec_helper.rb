7a93cc2c4c9d9f29b32cc7d0fc27d1610d2beab7
Eric Mueller <nevinera@gmail.com>
2023-04-24 13:31:00 -0400

    sort the glob before requiring (for consistent require order)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require "rspec"
 require File.expand_path("../../lib/environment_helpers", __FILE__)
 gem_root = File.expand_path("../..", __FILE__)
 support_glob = File.join(gem_root, "spec", "support", "**", "*.rb")
-Dir[support_glob].each { |f| require f }
+Dir[support_glob].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.raise_errors_for_deprecations!
