b766c06dde3c33f119d25dfa844edd28c7344846
Eric Mueller <nevinera@gmail.com>
2023-11-13 17:41:39 -0800

    fixup - add without_env

--- spec/support/with_environment.rb
+++ spec/support/with_environment.rb
@@ -10,6 +10,18 @@ module WithEnvironment
       end
     end
   end
+
+  def without_env(*keys)
+    around(:each) do |ex|
+      orig_env = ENV.to_h
+      keys.each { |key| ENV.delete(key) }
+      begin
+        ex.run
+      ensure
+        ENV.replace(orig_env)
+      end
+    end
+  end
 end
 
 RSpec.configure do |config|
