4f453a6ab3af44d907e90cd863867bb0028fac57
Eric Mueller <nevinera@gmail.com>
2025-07-07 19:43:58 -0400

    Handle non-conforming integer ENVs appropriately
    I must have _intended_ this behavior originally, because I defined
    EnvironmentHelpers::InvalidIntegerText quite a while ago. But nothing
    used it until now.

--- lib/environment_helpers/numeric_helpers.rb
+++ lib/environment_helpers/numeric_helpers.rb
@@ -2,8 +2,20 @@ module EnvironmentHelpers
   module NumericHelpers
     def integer(name, default: nil, required: false)
       check_default_type(:integer, default, Integer)
-      text = fetch_value(name, required: required)
+      text = fetch_value(name, required: required)&.strip
+      text = enforced_format(text: text, required: required)
       text&.to_i || default
+    end
+
+    private
+
+    def enforced_format(text:, required:)
+      return nil unless text
+      return text if text.match?(/\A-?\d+\z/)
+
+      # Not a "correct" integer (we accept only limited forms here)
+      return nil unless required
+      fail(InvalidIntegerText, "The environment vaiable '${name}' contains text that does not look like an integer")
     end
   end
 end
